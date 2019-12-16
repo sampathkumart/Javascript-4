fd924f669df3ff8a35bd0f8f9238f4d73a907246
sampathkumar <semsampathkumar@gmail.com>
2019-12-16 19:03:33 +0530

    variables

---
+++ variables/var.js
@@ -0,0 +1,8 @@
+var job = "teacher";
+console.log(job);
+
+// Cannot be reserved keyword
+// should be meaningful
+// Cannot start with a number
+// Cannot contain a space or hypen (-)
+// Are case-sensitive
