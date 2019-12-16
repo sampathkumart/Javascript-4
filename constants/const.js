bab769edcc90dc3160876dccb88b4e0f8e154662
sampathkumar <semsampathkumar@gmail.com>
2019-12-16 21:38:02 +0530

    primitive types

--- constants/const.js
+++ constants/const.js
@@ -1,3 +1,9 @@
-const price = 2500;
-price = 2555;
-console.log(price);
+// const price = 2500;
+// price = 2555;
+// console.log(price);
+
+console.log(a);
+var a = 10;
+
+console.log(b);
+let b = 15;
---
+++ primitive-types/primitive.js
@@ -0,0 +1,5 @@
+let job = "teacher"; // String literal
+let price = 46; // Number literal
+let cricketer = true; // Boolean literal
+let name = undefined; // Undefined
+let pickTeam = null; // Null
