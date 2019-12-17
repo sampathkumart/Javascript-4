f1d67558eb32fa7fee861f29edd1753721a5a764
sampathkumar <semsampathkumar@gmail.com>
2019-12-17 13:17:26 +0530

    types of functions

--- function/function.js
+++ function/function.js
@@ -1,6 +1,5 @@
 function hello(name, year) {
-  console.log("my name is " + name + " and my age is " + year);
+  console.log("my name is " + name + " and  age is " + year);
 }
 hello("virat", 30);
-
 hello("aswin", 46);
---
+++ types-of-function/func.js
@@ -0,0 +1,6 @@
+function multiple(number) {
+  return number * number;
+}
+multiple(46);
+console.log(multiple);
+//console.log(multiple(10));
