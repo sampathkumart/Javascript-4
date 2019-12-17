5e2fd41f2d7e2654a3f20b9dc6d767a87251436a
sampathkumar <semsampathkumar@gmail.com>
2019-12-17 19:29:06 +0530

    equality operator

---
+++ equality-operators/equality.js
@@ -0,0 +1,9 @@
+// Strict Equality (Type + Value)
+console.log(45 === 45);
+console.log(6 === "6");
+console.log(true === true);
+
+// Lose Equality (value)
+console.log(1 == 1);
+console.log(46 == "46");
+console.log(true == 1);
