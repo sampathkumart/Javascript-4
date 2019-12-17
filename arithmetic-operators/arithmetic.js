29da595182d1adefc4723afa689d6b08c6b97290
sampathkumar <semsampathkumar@gmail.com>
2019-12-17 14:33:31 +0530

    arithmetic opertors

---
+++ arithmetic-operators/arithmetic.js
@@ -0,0 +1,16 @@
+var v = 5;
+var d = 10;
+console.log(v + d);
+console.log(v * d);
+console.log(v - d);
+console.log(v % d);
+console.log(v / d);
+console.log(v ** d);
+
+// Increment (++)
+console.log(v++);
+console.log(v);
+
+// Decrement (--)
+console.log(d--);
+console.log(d);
