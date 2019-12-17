a451f537007d8a72c545dc84bc7afe3e9e1189c1
sampathkumar <semsampathkumar@gmail.com>
2019-12-17 10:41:43 +0530

    objects

---
+++ objects/objects.js
@@ -0,0 +1,13 @@
+var details = {
+  name: "sachin",
+  age: 40,
+  isMarried: true
+};
+// Dot notation
+details.age = 45;
+
+// Bracket Notation
+let change = "age";
+details[change] = 46;
+
+console.log(details.age);
