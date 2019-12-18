74374c64f47ade951530a572276c52d0afdcbe6c
sampathkumar <semsampathkumar@gmail.com>
2019-12-18 12:06:57 +0530

    switch statement

---
+++ switch-statement/switch.js
@@ -0,0 +1,17 @@
+var game = "army";
+
+switch (game) {
+  case "football":
+    console.log("he is a football player");
+    break;
+  case "cricket":
+    console.log("he is a cricket player");
+    break;
+
+  case "vollyball":
+    console.log("he is volleyball player");
+    break;
+
+  default:
+    console.log("he is a actor");
+}
