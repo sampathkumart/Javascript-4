01c581af09b79cc98fd6276734433e01bd5b3cf1
sampathkumar <semsampathkumar@gmail.com>
2019-12-20 12:31:42 +0530

    for in

---
+++ for-in-loops/for-in.js
@@ -0,0 +1,12 @@
+// for-in
+let details = {
+  name: "dhoni",
+  age: 38,
+  role: "batsman-wk"
+};
+
+for (let key in details) console.log(key, details[key]);
+
+var city = ["chennai", "mumbai"];
+
+for (var index in city) console.log(index, city[index]);
