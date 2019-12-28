ff9d1492958b774c0bd288f4fc20091efba39387
sampathkumar <semsampathkumar@gmail.com>
2019-12-28 11:59:09 +0530

    remove elements in array

---
+++ removing-elements(array)/remove.js
@@ -0,0 +1,15 @@
+//removing elements in array
+const balls = [6, 12, 18, 25];
+console.log(balls);
+
+//End
+const last = balls.pop();
+console.log(last);
+
+//Beginning
+const first = balls.shift();
+console.log(first);
+
+//Middle
+const middle = balls.splice(1);
+console.log(middle);
