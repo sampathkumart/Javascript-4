e58f70a92acdcac54f748a50d9fb14cd66e3b267
sampathkumar <semsampathkumar@gmail.com>
2019-12-28 09:31:19 +0530

    finding elements (primitives)

---
+++ finding-elements(primitives)/array.js
@@ -0,0 +1,4 @@
+const num = [45, 65, 65, 6545, "india"];
+console.log(num.indexOf(65));
+console.log(num.lastIndexOf(65));
+console.log(num.indexOf("india"));
--- string/string.js
+++
@@ -1,8 +0,0 @@
-//String primitive
-var bat = "mrf";
-console.log(typeof bat);
-console.log(bat.length);
-
-//String Object
-var ball = new String("boost ball");
-console.log(typeof ball);
